c769ceb2e512f2e19a67d27530f9dc79f4779c21
sunny hansda <sunnyhansda738@gmail.com>
2025-07-13 22:31:41 +0530

    my fifth commit

--- include iostream.c
+++ include iostream.c
@@ -3,10 +3,15 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "first commit" << std::endl;
+<<<<<<< HEAD
     std::cout << "try third commit" << std::endl;
     std::cout << "try fourth commit" << std::endl;
     
 
+=======
+    std::cout << "try second commit" << std::endl;
+    std::cout << "try fifth commit" << std::endl;
+>>>>>>> 6a20384 (my fifth commit)
     return 0;
 }
 
