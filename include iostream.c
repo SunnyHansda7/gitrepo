2a82fb81030cacd6c53765d5b1e95d4b2b32b914
sunny hansda <sunnyhansda738@gmail.com>
2025-07-13 22:06:06 +0530

    my remote to local merged

--- include iostream.c
+++ include iostream.c
@@ -3,6 +3,8 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "first commit" << std::endl;
+    std::cout << "try third commit" << std::endl;
+    std::cout << "try fourth commit" << std::endl;
     return 0;
 }
 
