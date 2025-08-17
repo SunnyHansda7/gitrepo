5aa2e6baa1efbab2f97980e374798465377675e6
sunny hansda <sunnyhansda738@gmail.com>
2025-08-17 22:44:14 +0530

    add eight commit.

--- include iostream.c
+++ include iostream.c
@@ -3,14 +3,12 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "first commit" << std::endl;
-
     std::cout << "try third commit" << std::endl;
     std::cout << "try fourth commit" << std::endl;
     std::cout << "try sixth commit" << std::endl;
-
     std::cout << "try second commit" << std::endl;
     std::cout << "try fifth commit" << std::endl;
-
+    std::cout << "try eight commit" << std::endl;
     return 0;
 }
 
