2a59c86b5f19856006e1324736e6bc867b553b58
sunny hansda <sunnyhansda738@gmail.com>
2025-08-17 21:42:05 +0530

    resolve git commit

--- include iostream.c
+++ include iostream.c
@@ -3,15 +3,14 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "first commit" << std::endl;
-<<<<<<< HEAD
+
     std::cout << "try third commit" << std::endl;
     std::cout << "try fourth commit" << std::endl;
     std::cout << "try sixth commit" << std::endl;
 
-=======
     std::cout << "try second commit" << std::endl;
     std::cout << "try fifth commit" << std::endl;
->>>>>>> 6a20384 (my fifth commit)
+
     return 0;
 }
 
